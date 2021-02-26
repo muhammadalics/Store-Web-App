caef0d7ba515843cb0230cabe8dc27dfd58a9b7c
Muhammad Ali <muhammadali.pete@gmail.com>
2021-02-26 15:42:21 -0700

    basic navigation set up

--- crwn-clothing/src/components/menu-item/menu-item.component.jsx
+++ crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
+
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl}) => (
-    <div 
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+    <div
+         
         style={{
             backgroundImage: `url(${imageUrl})`
         }}
 
-    className='menu-item'>
+    className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='content'>
-            <h1 className='title'>{title}</h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
